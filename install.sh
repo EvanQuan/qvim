528816c61c64108b025d488f274b9953a5eb4bb4
Evan Quan <evanlquan@gmail.com>
2018-05-17 23:29:25 -0600

    Add many settings from Bram Moolenaar's defaults.vim <Bram@vim.org> (2016 Sep 02), rename vimrc to vimrc.vim to further avoid confusion from .vimrc, add hard mode for arrow keys, pageup, pagedown, improve documentation

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # Name: install.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version: 1.0.0
+# Version: 1.0.1
 #
 # Initial installation setup
 
 # Update all
 bash ~/.vim/update_all.sh
 
-# Link .vimrc with vimrc
-echo "source ~/.vim/vimrc" >> ~/.vimrc
+# Link .vimrc with vimrc.vim
+echo "source ~/.vim/vimrc.vim" >> ~/.vimrc
