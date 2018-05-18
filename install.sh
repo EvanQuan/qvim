db00d18108e0fbee9ac2a015f925090f38b64f4a
Evan Quan <evanlquan@gmail.com>
2018-05-18 00:39:14 -0600

    ...and again

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Name: install.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version: 1.0.2
+# Version: 1.0.3
 #
 # Initial installation setup
 
@@ -8,4 +8,4 @@
 bash ~/.vim/complete_update.sh
 
 # Link .vimrc with vimrc.vim
-echo "source ~/.vim/vimrc.vim" >> ~/.vimrc
+echo "source ~/.vim/vimrc.vim" > ~/.vimrc
