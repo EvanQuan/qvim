32b080c50874d43017e558a423315c0ac9529d25
Evan Quan <evanlquan@gmail.com>
2018-05-23 12:38:10 -0600

    Rename vimrc.vim to vimrc to match with 2nd user vimrc file naming staandard

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # Name: install.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version: 1.0.3
+# Version: 1.0.4
 #
 # Initial installation setup
 
 # Update all
 bash ~/.vim/complete_update.sh
 
-# Link .vimrc with vimrc.vim
-echo "source ~/.vim/vimrc.vim" > ~/.vimrc
+# Link .vimrc with vimrc
+echo "source ~/.vim/vimrc" > ~/.vimrc
