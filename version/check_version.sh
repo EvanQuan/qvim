fb946d989cb60e666bb4c60b1259e99664e30675
Evan Quan <evanlquan@gmail.com>
2018-06-01 00:33:30 -0600

    Add 'almost-finished' check_version.sh

--- version/check_version.sh
+++ version/check_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Name:       check_version.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version:    1.0.0
+# Version:    0.9.0
 #
 # Check File Versions
 
@@ -10,6 +10,9 @@ printf "Updating local files:\n"
 local_versions=~/.vim/version/local/
 remote_versions=~/.vim/version/remote/
 remote_paths=~/.vim/version/path/
+remote_templates=~/.vim/version/templates/
+check_mark=✔
+cross_mark=✗
 
 # Create local versions if they do not exist
 if ! [ -d "$local_versions" ]; then
@@ -18,8 +21,7 @@ if ! [ -d "$local_versions" ]; then
 fi
 
 # Debug
-printf "\tIterating through: $remote_versions\n"
-let count=0
+# let count=0
 # Check every file that needs to be updated
 pushd "$remote_versions" > /dev/null
 for file in *; do
@@ -30,27 +32,42 @@ for file in *; do
     # remote_file=~/.vim/version/remote/$file
     remote_file="$remote_versions$base_name"
     path="$remote_paths$base_name"
+    template="$remote_templates$base_name"
 
     # Debug
-    printf "\n\t$count\n\n"
-    printf "\tbase_name: $base_name\n"
-    printf "\tlocal_file: $local_file\n"
-    printf "\tremote_file: $remote_file\n"
-    printf "\tpath: $path\n\n"
+    # printf "\n\t$count\n\n"
+    # printf "\tbase_name: $base_name\n"
+    # printf "\tlocal_file: $local_file\n"
+    # printf "\tremote_file: $remote_file\n"
+    # printf "\tpath: $path\n\n"
 
-    if ! [ -f $local_file ]; then
-        # If the local version does not exist, create it and update the file
-        # cp $remote_file $local_file
-        # bash update_file.sh $file $path
-        printf "\t$file [VERSION NOT FOUND] -> [$(<$remote_file)]\n"
-    elif ! [ -f $remote_file ]; then
-        printf "\t$file[REMOTE] does not exist. You dun goof'd\n"
-    elif diff $local_file $remote_file > /dev/null; then
-        # If the local version is different, update the version and file
-        printf "\t$file [$(<$local_file)] -> [$(<$remote_file)]\n"
-        # bash update_file.sh $file $path
+    if ! [ -f $remote_file ]; then
+        printf "\t$cross_mark $file remote version not found. Update failed!\n"
+    elif ! [ -f $path  ]; then
+        printf "\t$cross_mark $file remote path not found. Update failed!\n"
+    else
+        # Update
+        if [ -f $template ]; then
+            cp $template $path
+        else
+            # Debug
+            printf "\t$file does not have a template\n"
+        fi
+        if ! [ -f $local_file ]; then
+            # If the local version does not exist, create it and update the file
+            cp $remote_file $local_file
+            printf "\t$check_mark $file local version not found -> [$(<$remote_file)] Updated!\n"
+        elif diff $local_file $remote_file > /dev/null; then
+            # If the local version is different, update the version and file
+            cp $remote_file $local_file
+            printf "\t$check_mark $file [$(<$local_file)] -> [$(<$remote_file)]\n"
+            # bash update_file.sh $file $path
+        else
+            # Already updated
+            printf "\t$check_mark $file [$(<$remote_file)] Already up-to-date."
+        fi
     fi
-    let count=count+1
+    # let count=count+1
 done
 
 # Store popd so it doesn't printf to screen
