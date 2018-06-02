db1b3e16500db073b5ff7b33475768c43ed430f9
Evan Quan <evanlquan@gmail.com>
2018-06-02 01:39:08 -0600

    Add version test files

---
+++ version/test.sh
@@ -0,0 +1,9 @@
+# Create different files with different text
+echo "PASS" > a.txt
+echo "FAIL" > b.txt
+# echo "~/.vim/version/b.txt" > path.txt
+echo "./b.txt" > path.txt
+# Try to copy a.txt contents into b.txt using path.txt contents
+cp a.txt $(< path.txt)
+
+echo $(< b.txt)
