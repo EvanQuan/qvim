2495881ffe8f103fc7695c72a852d1fa37691a55
Evan Quan <evanlquan@gmail.com>
2018-06-21 12:53:29 -0600

    pull.sh 2.1.2 Revert submodule updating to use git submodule update. Change --rebase flag to --merge.

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # File:       pull.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version:    2.1.1
+# Version:    2.1.2
 #
 # Pull
 
@@ -10,15 +10,14 @@ cd ~/.vim
 # .vim
 git pull origin master
 # submodules
-# This, while simpler, can cause HEAD to detach, which is annoying
-# git submodule update --init --remote --rebase --recursive
+git submodule update --init --remote --merge --recursive
 
-# This, while printing more by going through all the submodules, prevents
-# HEAD from detaching
-git submodule update
-git submodule foreach git reset --hard
-git submodule foreach git checkout master
-git submodule foreach git pull origin master
+# This, while printing more and taking more time by going through all the
+# submodules, prevents HEAD from detaching
+# git submodule update
+# git submodule foreach git reset --hard
+# git submodule foreach git checkout master
+# git submodule foreach git pull origin master
 
 # Check file versions
 bash ~/.vim/version/check_version.sh
