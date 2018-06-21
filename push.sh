4f90ab74261908305326c39fff7a213a266bd4c2
Evan Quan <evanlquan@gmail.com>
2018-06-21 12:42:24 -0600

    push.sh 1.0.6 Set origin as upstream remote if not already done, and improve documentation.

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # File:       push.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version:    1.0.5
+# Version:    1.0.6
 #
 # Push
 
@@ -10,11 +10,11 @@ local_versions=~/.vim/version/local/
 remote_versions=~/.vim/version/remote/
 remote_templates=~/.vim/version/templates/
 
+# Iterate through all file versions to prompt user if they need updating
 pushd "$remote_versions" > /dev/null
 for file in $remote_versions/*; do
     # Get base name
     base_name=$(basename $file)
-    # remote_version=~/.vim/version/remote/$file
     local_version=$local_versions$base_name
     remote_version=$remote_versions$base_name
     printf "\nUpdate $base_name [$(<$remote_version)]:\n> "
@@ -32,16 +32,18 @@ done
 printf "\nStaging all changes..."
 # Store popd so it doesn't printf to screen
 trash=popd
-# Add everything
+# Add everything in repo directory
 git add ~/.vim
-printf "DONE\n"
+printf " DONE\n"
 
 # Get commit message
-# printf "\nCommit message:\n> "
-# read message
-# git commit -m "$message"
+# Used to have terminal prompt, but it did not allow for multiline messages
+#   printf "\nCommit message:\n> "
+#   read message
+#   git commit -m "$message"
 # Have vim open up so commmit message can be multiline
 git commit
 
-# # Push
-git push
+# Push
+# Add origin as the upstream remote if not already done
+git push -u origin master
