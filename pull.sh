8a3b9defb071a585f92bd1413529689af827425f
Evan Quan <evanlquan@gmail.com>
2018-06-28 02:21:58 -0600

    pull.sh 2.1.4 Revert submodule updating to old method to avoid HEAD detachment.

--- pull.sh
+++ pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # File:       pull.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version:    2.1.3
+# Version:    2.1.4
 #
 # Pull
 
@@ -10,14 +10,16 @@ cd ~/.vim
 # .vim
 git pull origin master
 # submodules
-git submodule update --init --remote --rebase --recursive
+#   --rebase causes detached HEAD
+#   --merge can sometimes fail to pull, rejected update
+# git submodule update --init --remote --rebase --recursive
 
 # This, while printing more and taking more time by going through all the
 # submodules, prevents HEAD from detaching
-# git submodule update
-# git submodule foreach git reset --hard
-# git submodule foreach git checkout master
-# git submodule foreach git pull origin master
+# git submodule update                          # Update as normal
+git submodule foreach git reset --hard          # Remove local changes
+git submodule foreach git checkout master       # Change to master branch
+git submodule foreach git pull origin master    # Update from master
 
 # Check file versions
 bash ~/.vim/version/check_version.sh
