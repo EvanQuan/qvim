8cc679b1b32a5ac1bd3d89ffa96be83b4454397d
Evan Quan <evanlquan@gmail.com>
2018-06-28 03:51:13 -0600

    check_version.sh 1.1.0 Use relative path and update formatting
    pull.sh 2.2.0
    Broken into this and pull_helper.sh
    
    pull_helper.sh 1.0.0
    Update submodules and execute check_version.sh

--- version/check_version.sh
+++ version/check_version.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 # Name:       check_version.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version:    1.0.3
+# Version:    1.1.0
 #
 # Keep local file versions up to date with remote versions.
+# Assumes this is being executed form pull_helper.sh in ~/.vim/version
+# or ~/vimfiles/version directory.
+
+# debug
+cd version
+
 
 printf "Updating local files:\n"
 
-local_versions=~/.vim/version/local/
-remote_versions=~/.vim/version/remote/
-remote_versions_files=~/.vim/version/remote/*
-remote_paths_absolute=~/.vim/version/path/  # Has to be absolute for path exist check to work
-remote_templates=~/.vim/version/templates/
+current_directory=$(pwd)
+local_versions=$current_directory/local/
+remote_versions=$current_directory/remote/
+remote_versions_files=$remote_versions*
+remote_paths_absolute=$current_directory/path/  # Has to be absolute for path exist check to work
+remote_templates=$current_directory/templates/
 check_mark=✔
 cross_mark=✗
+
+# printf "DEBUG Current directory: $current_directory\n"
+# printf "DEBUG local versions: $local_versions\n"
+# printf "DEBUG remote versions: $remote_versions\n"
+# printf "DEBUG remote paths aboslute: $remote_paths_absolute\n"
+# printf "DEBUG remote templates: $remote_templates\n"
 
 # Create local versions directory if they do not exist
 # Directory will now be empty, and the following loop will
@@ -25,25 +38,40 @@ if ! [ -d "$local_versions" ]; then
 fi
 
 # Check every file that needs to be updated
-pushd "$remote_versions" > /dev/null
-for file in *; do
+# pushd "$remote_versions" > /dev/null
+for file in $remote_versions_files; do
+    # printf "DEBUG file: $file\n"
     # Get base name
     base_name=$(basename $file)
+    # printf "DEBUG base name: $base_name\n"
     # local_version=~/.vim/version/local/$file
     local_version=$local_versions$base_name
+    # printf "DEBUG local version: $local_version\n"
     # remote_version=~/.vim/version/remote/$file
     remote_version=$remote_versions$base_name
+    # printf "DEBUG remote version: $remote_version\n"
     absolute_path=$remote_paths_absolute$base_name
+    # printf "DEBUG absolute_path: $absolute_path\n"
     template=$remote_templates$base_name
+    # printf "DEBUG template: $template\n"
 
     if ! [ -f $remote_version ]; then
-        printf "\t$cross_mark $file remote version not found. Update failed!\n"
+        update_symbol=$cross_mark
+        update_version="[???]"
+        update_message="Remote version not found. Update failed!"
+        # printf "\t$cross_mark \"$file\" remote version not found. Update failed!\n"
     elif ! [ -f $absolute_path ]; then
-        printf "\t$cross_mark $file remote path to not found. Update failed!\n"
+        update_symbol=$cross_mark
+        update_version="[???]"
+        update_message="Remote path not found. Update failed!"
+        # printf "\t$cross_mark \"$file\" remote path to not found. Update failed!\n"
     else
         if ! [ -f $local_version ]; then
             # If the local version does not exist, create it and update the file
-            printf "\t$check_mark $file local version not found -> [$(<$remote_version)] Updated!\n"
+            update_symbol=$check_mark
+            update_version="[$(<$remote_version)]"
+            update_message="Local version not found. Updated!"
+            # printf "\t$check_mark $file local version not found -> [$(<$remote_version)] Updated!\n"
 
             cp $remote_version $local_version # Update local version number
             # Update local file to match template
@@ -52,7 +80,10 @@ for file in *; do
             fi
         elif ! diff $local_version $remote_version > /dev/null; then
             # If the local version is different, update the version and file
-            printf "\t$check_mark $file [$(<$local_version)] -> [$(<$remote_version)] Updated!\n"
+            update_symbol=$check_mark
+            update_version="[$(<$local_version)] -> [$(<$remote_version)]"
+            update_message="Updated!"
+            # printf "\t$check_mark $file [$(<$local_version)] -> [$(<$remote_version)] Updated!\n"
 
             cp $remote_version $local_version # Update local version number
             # Update local file to match template
@@ -61,10 +92,13 @@ for file in *; do
             fi
         else
             # Already updated. Do not update local file.
-            printf "\t- $file [$(<$remote_version)] Already up-to-date.\n"
+            update_symbol="-"
+            update_version="[$(<$remote_version)]"
+            update_message="Already up-to-date"
         fi
+        printf "\t%s %-16s %-24s %s\n" "$update_symbol" "$base_name" "$update_version" "$update_message"
     fi
 done
 
 # Store popd so it doesn't printf to screen
-trash=popd
+# trash=popd
---
+++ version/path/pull_helper.sh
@@ -0,0 +1,1 @@
+../pull_helper.sh
