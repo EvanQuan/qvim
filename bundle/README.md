dc73418ffc3489dd459043ed1194f5911990de04
Evan Quan <evanlquan@gmail.com>
2018-11-27 13:45:34 -0700

    vimrc 2.0.0, pull_helper.sh 2.0.0
    vimrc
    Use vim-plug instead of pathogen.
    
    pull_helper.sh
    No longer updates submodules as there are no longer any submodules.
    
    README.md
    Update install and update instructions for vim-plug use.
    Credit vim-plug, vim-endwise.

--- bundle/README.md
+++
@@ -1,4 +0,0 @@
-The `bundle` directory contains plugins managed by the pathogen plugin, where
-each plugin is treated as a submodule in the Git repository.
-At some point, pathogen may be phased out, where the plugins will be managed
-under Vim 8's native plugin manager.
--- version/pull_helper.sh
+++ version/pull_helper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # File:       pull_helper.sh
 # Maintainer: https://github.com/EvanQuan/.vim/
-# Version:    1.0.2
+# Version:    2.0.0
 #
 # Pull Helper
 # If pulling implementation changes are made, they are made here to allow
@@ -9,23 +9,6 @@
 # properly.
 # This is done as pull.sh pulls from master branch first, which would update
 # this file first before executing this file.
-
-
-# Update submodules
-#   Alternative method.
-#   Has nicer output (only prints changes).
-# git submodule update --init --remote --merge --recursive
-#   Problems
-#   --rebase causes detached HEAD
-#   --merge can sometimes fail to pull due to local changes, and reject update
-
-# This, while printing more and taking more time by going through all the
-# submodules, prevents HEAD from detaching
-git submodule update  --init                    # Initialize if not already done
-git submodule foreach git checkout master       # Change to master branch
-# git submodule foreach git fetch --all           # get latest from remote
-# git submodule foreach git reset --hard          # Remove local changes
-git submodule foreach git pull origin master    # Update from master
 
 # Change directory to make changes path relative so it works for both
 # ~/.vimrc and ~/vimfiles
